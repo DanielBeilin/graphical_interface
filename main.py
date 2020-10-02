24a7c04de59e69bfc79265dd36edf5d759737dec
DanielBeilin <dbeilin3@gmail.com>
2020-10-03 00:51:08 +0300

    change from kd_node to node

--- main.py
+++ main.py
@@ -28,9 +28,9 @@ def BuildTree(points, i=0):
 NULL = [0, (0,0)]
 
 # finds nearest point to given line from the right
-def NearestRightPoint(kd_node, line, best=None):
-    if kd_node is not None:
-        curr_root = kd_node[2]
+def NearestRightPoint(node, line, best=None):
+    if node is not None:
+        curr_root = node[2]
         dist = curr_root[0] - line
         if dist > 0:
             if not best:
@@ -38,8 +38,8 @@ def NearestRightPoint(kd_node, line, best=None):
             elif dist < best[0]:
                 best = [dist, curr_root]
         direction = 0 if curr_root[0] > line else 1
-        if kd_node[direction]:
-            return NearestRightPoint(kd_node[direction], line, best)
+        if node[direction]:
+            return NearestRightPoint(node[direction], line, best)
     return best if best else NULL
 
 def main():
